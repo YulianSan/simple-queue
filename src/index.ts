7b978bcf39fe40774772f39f6690c29a2bfcf544
YulianSan <knoob0733@gmail.com>
2023-12-31 19:18:14 -0300

    feat(index): adding basic logic, I'll improve later

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Consumer } from "./Consumer/Consumer"
 import { Message } from "./Message/Message"
+import { Queue } from "./Queue/Queue"
+import { Queues } from "./Queues/Queues"
 
-let consumers: Consumer[] = []
-let queues: Message[] = []
+let queues: Queues = new Queues()
 
 Bun.serve<any>({
 	async fetch(req, server) {
@@ -19,14 +20,33 @@ Bun.serve<any>({
 		if (server.upgrade(req, { data })) return;
 
 		try {
+			const contentType = req.headers.get('content-type')
+
+			if (contentType !== 'application/json') {
+				return Response.json(
+					{ message: 'Content type not supported' },
+					{ status: 400 }
+				)
+			}
+
 			const body = await req.json()
 
-			queues.push(new Message(crypto.randomUUID(), {
+			let message = new Message(crypto.randomUUID(), {
 				queue: queue,
 				data: body,
-			}))
+			})
 
-			consumers[0].sendMessage(queues[queues.length - 1])
+			if (!queues.getQueue(queue))
+				queues.addQueue(queue, new Queue())
+
+			queues.getQueue(queue).enqueue(message)
+
+			let consumer = queues.getConsumerByCallback((consumer) => consumer.available)
+
+			if (consumer) {
+				let queueC = queues.getQueue(queue).dequeue()
+				consumer.sendMessage(queueC)
+			}
 
 			return new Response('', { status: 201 });
 		} catch (e: any) {
@@ -37,45 +57,44 @@ Bun.serve<any>({
 				)
 			}
 			console.log(e)
+			return Response.json(
+				{ message: 'Request Error' },
+				{ status: 400 }
+			)
 		}
-		return Response.json(
-			{ message: 'Request Error' },
-			{ status: 400 }
-		)
-
-
 	},
 	websocket: {
 		open(ws) {
-			consumers.push(new Consumer(ws.data.consumerId, ws))
-			ws.subscribe(ws.data.queue);
+			queues.addConsumer(new Consumer(ws.data.consumerId, ws))
+			let queue = queues.getQueue(ws.data.queue)?.dequeue()
 
-			const openMsg = `new consumer: ${ws.data?.consumerId ?? 'guest'}`
-			console.log(openMsg)
+			if (!queue) return
+
+			queues.getConsumerByCallback(
+				(consumer) => consumer.id === ws.data.consumerId
+			)?.sendMessage(queue)
+
 		},
-
 		message(ws, message) {
 			try {
 				const messageJson: any = JSON.parse(message.toString())
 				if (messageJson?.success) {
-					queues = queues.filter((queue) =>
-						queue.id !== consumers.find((consumer) =>
-							consumer.id === ws.data.consumerId
-						)?.messageId
-					)
+					queues.success(ws.data.consumerId)
+
+					let queue = queues.getQueue(ws.data.queue).dequeue()
+
+					if (!queue) return
+
+					queues.getConsumerByCallback(
+						(consumer) => consumer.id === ws.data.consumerId
+					)?.sendMessage(queue)
 				}
 			} catch (e: any) {
 				console.log(e)
 			}
 		},
-
 		close(ws) {
-			ws.unsubscribe(ws.data.queue);
-			console.log(`Disconnect consumer: ${ws.data.consumerId}`)
-
-			consumers = consumers.filter((consumer) => {
-				return consumer.id != ws.data.consumerId
-			})
+			queues.removeConsumer(ws.data.consumerId)
 		},
 	},
 	port: 3000
